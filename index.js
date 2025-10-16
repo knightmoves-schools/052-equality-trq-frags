f088e067c365b771935c672244013d094594af0e
track-frags <jadirks@anamosa.k12.ia.us>
2025-10-16 17:02:40 +0000

    i need help. 4 lessons today.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function priceCheck(price) {
+  if (price == 1.50) {
+    return "match";
+  }
+}
 
+function strictPriceCheck(price) {
+  if (price === 1.50) {
+    return "match";
+  }
+}
+
+// ...please help me survive
